559e00d7da8e8ac05eee9d96cc9eebf12deff9f0
Italo M <italomarcos0010@gmail.com>
2020-08-21 11:34:51 -0300

    lol

--- src/components/Line/index.js
+++ src/components/Line/index.js
@@ -1,14 +1,13 @@
 import styled from 'styled-components';
 
 export default styled.div`
-  width: 906px;
   display: flex;
   justify-content: space-between;
-  height: 76px;
   align-items: center;
   margin-top: 20px;
 
   flex-direction: ${({ isDesktop }) => (isDesktop ? 'row' : 'column')};
   background-color: ${({ isDesktop }) => (isDesktop ? '#fff' : '#f0f')};
   height: ${({ isDesktop }) => (isDesktop ? 76 : 248)}px;
+  width: ${({ isDesktop }) => (isDesktop ? '906px' : '308px')};
 `;
