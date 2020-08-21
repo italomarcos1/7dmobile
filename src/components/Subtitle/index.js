783050193e3570cc043532214f7f994bacc35e48
Italo M <italomarcos0010@gmail.com>
2020-08-21 13:05:34 -0300

    a

--- src/components/Subtitle/index.js
+++ src/components/Subtitle/index.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export default styled.div`
   text-align: left;
   padding: 0;
-  padding-left: 10px;
+  padding-left: ${({ isDesktop }) => (isDesktop ? 0 : 10)}px;
   font-size: 13px 'Roboto';
   letter-spacing: 0.28px;
   color: #aaa;
